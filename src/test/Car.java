8bfee53e0cd6efaa3f2e45c35acf8b90b5195b7e
sblick19 <sophiecblick@gmail.com>
2024-02-23 00:33:55 -0600

    add functions and change return statement to car

--- src/test/Car.java
+++ src/test/Car.java
@@ -7,5 +7,22 @@ public class Car extends Vehicle{
 		super(brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public double calculateMaintenanceCost(double distance) {
+        // Implementation specific to calculating maintenance cost for a car
+        return (distance * mass * (2024 - modelYear)* cylinders * 0.0005);
+    }
+
+
+    public double calculateFuelEfficiency(double distance, double fuelPrice) {
+        // Implementation specific to calculating fuel efficiency for a car
+        return (cylinders * gasTankCapacity * fuelPrice / distance * 0.003);
+    }
+
+    
+    public void startEngine() {
+    	// prints how the vehicle starts
+        System.out.println("startType=" + startType);
+    }
 
 }
