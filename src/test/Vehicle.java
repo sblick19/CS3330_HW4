e1ff87129ff93263c45e4b4fa4c6244a89fd8c7b
sblick19 <sophiecblick@gmail.com>
2024-02-23 00:10:54 -0600

    add constructor

--- src/test/Vehicle.java
+++ src/test/Vehicle.java
@@ -25,4 +25,21 @@ public abstract class Vehicle {
     protected int cylinders;
     protected double gasTankCapacity;
     protected StartMechanism startType;
+    
+    //constructor
+    public Vehicle(String brand, String make, long modelYear, double price, VehicleColor color,
+            FuelType fuelType, double mileage, double mass, int cylinders,
+            double gasTankCapacity, StartMechanism startType) {
+    	this.brand = brand;
+    	this.make = make;
+    	this.modelYear = modelYear;
+    	this.price = price;
+    	this.color = color;
+    	this.fuelType = fuelType;
+    	this.mileage = mileage;
+    	this.mass = mass;
+    	this.cylinders = cylinders;
+    	this.gasTankCapacity = gasTankCapacity;
+    	this.startType = startType;
+}
 }
