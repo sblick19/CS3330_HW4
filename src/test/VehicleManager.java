d4e331eda9b4a97b08a709fb0a642af1da349014
djhkbw <djhkbw@umsystem.edu>
2024-02-23 22:23:46 -0600

    Initialize variables and methods in VehicleManager

--- src/test/VehicleManager.java
+++ src/test/VehicleManager.java
@@ -1,5 +1,76 @@
 package test;
 
 public class VehicleManager {
+	
+	private String vehicleFilePath;
+	private ArrayList<Vehicle> vehicleList = new ArrayList <Vehicle>();
+	
+	public boolean initializeStock() {
+
+	}
+	
+	public void displayAllCarInformation() {
+		
+	}
+	
+	public void displayAllTruckInformation() {
+		
+	}
+	
+	public void displayAllSUVInformation() {
+		
+	}
+	
+	public void displayAllMotorBikeInformation() {
+		
+	}
+	
+	public void displayVehicleInformation(Vehicle v) {
+		
+	}
+	
+	public void displayAllVehicleInformation() {
+		
+	}
+	
+	public boolean removeVehicle(Vehicle vehicle) {
+		
+	}
+	
+	public boolean addVehicle(Vehicle vehicle) {
+		
+	}
+	
+	public boolean saveVehicleList() {
+		
+	}
+	
+	private boolean isVehicleType(Vehicle v, Class clazz) {
+		
+	}
+	
+	public int getNumberOfVehichlesByType(Class clazz) {
+		
+	}
+	
+	public Vehicle getVehicleWithHighestMaintenanceCost(double distance) {
+		
+	}
+	
+	public Vehicle getVehicleWithLowestMaintenanceCost(double distance) {
+		
+	}
+	
+	public ArrayList<Vehicle> getVehicleWithHighestFuelEfficiency(double distance, double fuelPrice){
+		
+	}
+	
+	public ArrayList<Vehicle> getVehicleWithLowestFuelEfficiency(double distance, double fuelPrice){
+		
+	}
+	
+	public double getAverageFuelEfficiencyOfSUVs(double distance, double fuelPrice) {
+		
+	}
 
 }
