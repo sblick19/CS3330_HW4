aa4d6daf64fadeaec8637bbb47aefc45eccbc28b
Antedbell20 <78629236+Antedbell20@users.noreply.github.com>
2024-02-24 18:21:58 -0600

    added to the main

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,6 +4,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("testing that main works");
+		VehicleManager vm = VehicleManager.getInstance();
+		boolean success = vm.initializeStock();
+
+		if(success) {
+			System.out.println("Vehicle stock initialized successfully");
+			vm.displayAllCarInformation();
+			vm.displayAllTruckInformation();
+			vm.displayAllSUVInformation();
+			vm.displayAllMotorBikeInformation();
+			vm.displayVehicleInformation(null);
+			vm.displayAllVehicleInformation();
+			vm.removeVehicle(null);
+			vm.addVehicle(null);
+			vm.saveVehicleList();
+			vm.getNumberOfVehiclesByType(null);
+			
+
+
+			
+
+		} else {
+			System.out.println("Vehicle stock initialization failed");
+		}
 
 	}
 
